552bfc09b635721521a8bcde03f5e4d5d8553ddc
Xavier MAQUET <xmaquet@orange.fr>
2025-11-15 15:45:22 +0100

    fix(bdx): import OnnxInfer optionnel pour Ã©viter onnxruntime Ã  lâ€™import du package

--- mini_bdx_runtime/mini_bdx_runtime/__init__.py
+++ mini_bdx_runtime/mini_bdx_runtime/__init__.py
@@ -1,1 +1,7 @@
-from .onnx_infer import OnnxInfer
+# Éviter d'imposer onnxruntime à l'import du package.
+# Import paresseux: les utilisateurs qui en ont besoin importent directement depuis le sous-module.
+try:
+	from .onnx_infer import OnnxInfer  # type: ignore
+except Exception:
+	# Optionnel: laisser non défini pour ne pas créer de dépendance dure
+	OnnxInfer = None  # type: ignore
